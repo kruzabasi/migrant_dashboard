869e3efe5e785d483050377917cffd9d6df2e99c
kruzabasi <kruzabasi@gmail.com>
2020-05-20 11:50:31 +0100

    bootstrap styling

---
+++ src/Board.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./index.css";
+
+class Board extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      butterQuantity: 67,
+      butterPrice: 455,
+      sugarCrates: 44,
+      sugarQuantity: 400,
+      isLoggedIn: true
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="card shadow mb-3 rounded" style={{ width: 300 }}>
+          <div className="card-body">
+            <h5 className="card-title text-center">The Quantity of Butter</h5>
+            <p className="card-text text-center">{this.state.butterQuantity}</p>
+            <button className="butterButton1">Update Quantity</button>
+            <button className="butterButton2">Delete Value</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
